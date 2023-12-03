85b8ea9b72bc14c1ffc1794ce4d1e96f4815f5e1
FantomIT <129615173+fatumUser@users.noreply.github.com>
2023-12-03 15:41:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,16 @@ function fullImgDeliveryClose() {
     document.getElementById("full-img-delivery").classList.remove("full-img-show");
     document.getElementById("body").classList.remove("body-no-scroll");
 }
+setTimeout(screenSaver, 1800);
+setTimeout(screenSaverZindex, 2800);
+setTimeout(screenSaverMargin, 50);
+function screenSaver() {
+  document.getElementById("screen-saver").classList.add("hidden-screen-saver");
+}
+function screenSaverZindex() {
+    document.getElementById("screen-saver").classList.add("z-index-screen-saver");
+  }
+  function screenSaverMargin() {
+    document.getElementById("screen-saver-logo").classList.add("margin-screen-saver-logo");
+  }
+  
